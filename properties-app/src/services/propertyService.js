e88dd432fb8e9b4017a4c57d1609abdbf6158b74
fredyml <113043655+fredyml@users.noreply.github.com>
2025-01-15 07:49:01 -0500

    .

--- properties-app/src/services/propertyService.js
+++ properties-app/src/services/propertyService.js
@@ -1,12 +1,12 @@
+
 import axios from 'axios';
 
-// La URL base de la API
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = "https://localhost:7179/api";
 
-// Función para obtener propiedades basadas en los filtros
-export const getProperties = (filters = {}) =>
-  axios.get(`${API_BASE_URL}/properties`, { params: filters });
+export const getProperties = (filters = {}) => {
+  return axios.get(`${API_BASE_URL}/properties`, { params: filters });
+};
 
-// Función para obtener una propiedad por su ID
-export const getPropertyById = (id) =>
-  axios.get(`${API_BASE_URL}/properties/${id}`);
+export const getPropertyById = (id) => {
+  return axios.get(`${API_BASE_URL}/properties/${id}`);
+};
